0325db4afd79c592f35663c89ba7409712dba5d3
GRDev <gierremartins@gmail.com>
2024-09-11 00:16:31 -0300

    first commit

---
+++ src/app/404/page.tsx
@@ -0,0 +1,11 @@
+const Custom404 = () => {
+  return (
+    <div>
+      <h1>404 - Página Não Encontrada</h1>
+      <p>Oops! Parece que a página que você está procurando não existe.</p>
+      <a href="/">Voltar para a página inicial</a>
+    </div>
+  );
+};
+
+export default Custom404;
---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Contacts = () => {
+  return (
+    <div>
+      <h1>Contatos</h1>
+
+      <Link href="/">Home</Link>
+    </div>
+  );
+};
+
+export default Contacts;
